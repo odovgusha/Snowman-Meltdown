a110590799ff8331f421ec29004cfd5d6ac917a3
dovgusha1 <dovgusha1@gmail.com>
2026-01-07 03:09:42 +0100

    replayability added

--- game_logic.py
+++ game_logic.py
@@ -23,43 +23,48 @@ def display_game_state(mistakes, secret_word, guessed_letters):
 
 
 def play_game():
-    secret_word = get_random_word()
-    guessed_letters = []
-    mistakes = 0
-    max_mistakes = len(STAGES)-1
-
-    print("Welcome to Snowman Meltdown Game!")
-    print("Please do not melt before the Snowman does!")
-
     while True:
-        display_game_state(mistakes, secret_word, guessed_letters)
+        secret_word = get_random_word()
+        guessed_letters = []
+        mistakes = 0
+        max_mistakes = len(STAGES)-1
 
-        if all(letter in guessed_letters for letter in secret_word):
-            print("You won! Snowman may live...for now.")
+        print("Welcome to Snowman Meltdown Game!")
+        print("Please do not melt before the Snowman does!")
+
+        while True:
+            display_game_state(mistakes, secret_word, guessed_letters)
+
+            if all(letter in guessed_letters for letter in secret_word):
+                print("You won! Snowman may live...for now.")
+                break
+
+            if mistakes >= max_mistakes:
+                print("The Snowman is no more with us!")
+                break
+
+            guess = input("Guess a letter: ").lower()
+
+            if len(guess) != 1 or not guess.isalpha():
+                print("Please enter a letter.")
+                continue
+
+            if guess in guessed_letters:
+                print("You already guessed that letter.")
+                continue
+
+            guessed_letters.append(guess)
+
+            if guess not in secret_word:
+                mistakes += 1
+                print("Wrong guess!\n")
+            else:
+                print("Good guess!\n")
+
+        again = input("Play again? (y/n): ").lower()
+        if again != "y":
+            print("Thank you for playing!")
             break
-
-        if mistakes >= max_mistakes:
-            print("The Snowman is no more with us! Try again.")
-            break
-
-        guess = input("Guess a letter: ").lower()
-
-        if len(guess) != 1 or not guess.isalpha():
-            print("Please enter a letter.")
-            continue
-
-        if guess in guessed_letters:
-            print("You already guessed that letter.")
-            continue
-
-        guessed_letters.append(guess)
-
-        if guess not in secret_word:
-            mistakes += 1
-            print("Wrong guess!\n")
-        else:
-            print("Good guess!\n")
 
 if __name__ == "__main__":
     play_game()
-
